394df196d4e6aaa8730e75032d4bf8003ec3e2b7
Kevin Zhang <kevinzhang425@gmail.com>
2022-04-11 19:09:38 -0700

    Add space to trigger codecov

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     
     @RequestMapping("/")
-    public String index() {
+    public String index() { 
         return "Greetings from kevinzhang421!";
     }
     
